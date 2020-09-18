3195ddc950e8d62b6b151abc0b95d34f8e37a8c1
cjm143 <cjm143@naver.com>
2020-09-18 15:29:55 +0900

    messege

---
+++ index.js
@@ -0,0 +1,13 @@
+var 변수명 = 값;
+var 변수명 = 값 => var 변수명 = null;
+var food = {x:null, y:null};
+
+class랑 비슷한 개념의 생성자 함수
+//함수 function 생성자로 쓸거면 this를 써야함.
+function Snake(){
+    this.x = 10;
+}
+
+
+git config --global user.name "cjm143"
+git config --global user.email "cjm143@naver.com"
